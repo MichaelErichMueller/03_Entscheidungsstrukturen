c667e6478671e14570da333018dd574ac99ccdbb
Peter <schmidt.multimedia@gmail.com>
2022-11-10 12:20:49 +0100

    SWITCH|CASE 1

--- script_01.js
+++ script_01.js
@@ -55,18 +55,50 @@ isJohnEqual = (ageJohn == ageMark);
 /************ IF - ELSE IF  ************/
 // mit alternativen Fällen (älter, jünger, gleich alt)
 
-if(isJohnOlder)
-{
-    console.log("John ist älter.");
-} 
-// 1. Alternative  .... n
-else if(isJohnEqual)
-{
-    console.log("John ist gleich alt."); 
+// if(isJohnOlder)
+// {
+//     console.log("John ist älter.");
+// } 
+// // 1. Alternative  .... n
+// else if(isJohnEqual)
+// {
+//     console.log("John ist gleich alt."); 
+// }
+// // evtl. weitere Alternative(n) else if(){}
+// // gemeinsame Alternative
+// else 
+// {
+//     console.log("John ist jünger."); 
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein!
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+job = "instructor";  // .. unterrichtet!
+
+switch (job) {  // Test auf ==
+    case "driver":
+        console.log(firstName + " fährt TAXI!");
+        break;
+    case "diver":
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist":
+        console.log(firstName + " malt ein Bild!");
+        break;
+    case "teacher":
+    case "instructor":   
+        console.log(firstName + " unterrichtet!");
+        break;
+    default:
+        console.log(firstName + " macht etwas anderes!");
+        break;
 }
-// evtl. weitere Alternative(n) else if(){}
-// gemeinsame Alternative
-else 
-{
-    console.log("John ist jünger."); 
-}
+
